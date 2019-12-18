70e6c772413dfd8ea19da21620f576763e032529
Lee Bardon <leerbardon@gmail.com>
2019-12-18 16:39:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("#{element}")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.shift
+  array.shift(element)
 end
 
 def remove_element_from_start_of_array(array)
