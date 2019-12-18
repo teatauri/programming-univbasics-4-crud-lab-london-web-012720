56e286d66ac042a7878441cf4dcdbd46527f7c4a
Lee Bardon <leerbardon@gmail.com>
2019-12-18 16:42:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.pop
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  return array[1]
+  return array[0]
 end
 
 def retrieve_last_element_from_array(array)
